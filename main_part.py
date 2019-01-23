bad365abfd6970602d7933aadbd5e8922e57f6a1
Violetta <violetta.osipenkova@icloud.com>
2019-01-23 10:18:02 +0300

    ticks

--- main_part.py
+++ main_part.py
@@ -5,7 +5,6 @@ import pygame.display
 
 n_stone = [6, 10, 15, 21]
 
-
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
@@ -27,8 +26,6 @@ def start_game():
     screen.fill((255, 255, 255))
     running = True
     bg = Background('pictures/cave.png', [0, 0])
-    #  right = Background('pictures/right.png', [800, 646])
-    #  left = Background('pictures/left.png', [0, 650])
 
     mouse_sprites = pygame.sprite.Group()
     mouse = pygame.sprite.Sprite()
@@ -44,6 +41,24 @@ def start_game():
     left.image = load_im("pictures/left.png")
     left.rect = left.image.get_rect()
 
+    right_tick = pygame.sprite.Sprite()
+    right_tick.image = load_im("pictures/tick.png")
+    right_tick.rect = right_tick.image.get_rect()
+
+    left_tick = pygame.sprite.Sprite()
+    left_tick.image = load_im("pictures/tick.png")
+    left_tick.rect = left_tick.image.get_rect()
+
+    right.rect.x = 800
+    right.rect.y = 650
+    left.rect.x = 0
+    left.rect.y = 650
+
+    right_tick.rect.x = 750
+    right_tick.rect.y = 655
+    left_tick.rect.x = 200
+    left_tick.rect.y = 655
+
     numb = random.randint(0, 3)
     numb_move = random.randint(2, 5)
 
@@ -51,8 +66,8 @@ def start_game():
     stones_sp = []
 
     ky = 0
-
     player = 1
+    current = 0
 
     class Stone(pygame.sprite.Sprite):
         images = load_im("pictures/diamond.png")
@@ -66,17 +81,18 @@ def start_game():
         stones_sprites_group.append(pygame.sprite.Group())
         stones_sp.append(Stone(stones_sprites_group[i]))
 
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Extra Cheese Melted', 50)
+    curr_text = myfont.render('x' + str(current), False, (255, 255, 255))
+
     if numb == 0:
         ky = 350
-
     if numb == 1:
         ky = 300
-
     if numb == 2:
         ky = 200
-
     if numb == 3:
-        ky = 150
+        ky = 120
 
     ki = 0
     start_x = 450
@@ -103,6 +119,7 @@ def start_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
             if event.type == pygame.MOUSEMOTION:
                 if pygame.mouse.get_focused():
                     mouse.rect.x = event.pos[0]
@@ -111,7 +128,22 @@ def start_game():
                     for i in range(n_stone[numb]):
                         stones_sprites_group[i].draw(screen)
                     mouse_sprites.draw(screen)
+
             if event.type == pygame.MOUSEBUTTONDOWN:
+                current += 1
+
+                if player == 1:
+                    if current == numb_move:
+                        player = 2
+                        current = 0
+                    curr_text = myfont.render('x' + str(current), False, (255, 255, 255))
+
+                elif player == 2:
+                    if current == numb_move:
+                        player = 1
+                        current = 0
+                    curr_text = myfont.render('x' + str(current), False, (255, 255, 255))
+
                 for i in range(n_stone[numb]):
                     if stones_sp[i].rect.collidepoint(event.pos):
                         stones_sp[i].kill()
@@ -119,12 +151,15 @@ def start_game():
                         for i in range(n_stone[numb]):
                             stones_sprites_group[i].draw(screen)
                         mouse_sprites.draw(screen)
+
         if player == 1:
             screen.blit(left.image, left.rect)
+            screen.blit(curr_text, (80, 667))
+            screen.blit(left_tick.image, left_tick.rect)
         else:
             screen.blit(right.image, right.rect)
-        left.rect.x = 0
-        left.rect.y = 650
+            screen.blit(curr_text, (880, 667))
+            screen.blit(right_tick.image, right_tick.rect)
         pygame.display.flip()
     pygame.quit()
 
