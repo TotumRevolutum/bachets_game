9205498e48c02118b8497c16c05c0f2c67b4c8a6
Violetta <violetta.osipenkova@icloud.com>
2019-02-05 10:08:34 +0300

    version 1.2

--- ending.py
+++ ending.py
@@ -11,49 +11,57 @@ def load_image(name):
 def end_game(player):
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
-    screen.fill((240, 105, 5))
+    screen.fill((5, 0, 75))
+    screen_rect = (0, 0, 1000, 800)
 
-    class SnowFlake():
-        def __init__(self, size, position, wind=False):
-            self.size = size
-            self.position = position
-            self.wind = wind
+    class Particle(pygame.sprite.Sprite):
+        fire = [load_image("star.png")]
+        for scale in (5, 10, 20):
+            fire.append(pygame.transform.scale(fire[0], (scale, scale)))
 
-        def fall(self, speed):
-            self.position[1] += speed
+        def __init__(self, pos, dx, dy):
+            super().__init__(all_sprites)
+            self.image = random.choice(self.fire)
+            self.rect = self.image.get_rect()
 
-        def draw(self):
-            numb = (random.randint(20, 250))
-            color = (numb, 175, 252)
-            pygame.draw.circle(screen, color, self.position, self.size)
+            self.velocity = [dx, dy]
+            self.rect.x, self.rect.y = pos
+
+            self.gravity = 0.1
+
+        def update(self):
+            self.velocity[1] += self.gravity
+            self.rect.x += self.velocity[0]
+            self.rect.y += self.velocity[1]
+            if not self.rect.colliderect(screen_rect):
+                self.kill()
+
+    def create_particles(position):
+        particle_count = 7
+        numbers = range(-5, 6)
+        for _ in range(particle_count):
+            Particle(position, random.choice(numbers), random.choice(numbers))
 
     running = True
     pygame.font.init()
     myfont = pygame.font.Font('Monly-Light.otf', 100)
-    winner = myfont.render('Победитель: ' + str(player), False, (240, 105, 5))
+    winner = myfont.render('Победитель: ' + str(player), False, (5, 0, 75))
     clock = pygame.time.Clock()
-    snow_list = []
+    all_sprites = pygame.sprite.Group()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((240, 105, 5))
-        y_position = 0
-        x_position = random.randint(0, 1000)
-        speed = 1
-        y_position += speed
+        screen.fill((5, 0, 75))
 
-        flake = SnowFlake(5, [x_position, y_position], False)
+        create_particles((random.randint(0, 950), (random.randint(0, 750))))
+        all_sprites.update()
 
-        snow_list.append(flake)
-
-        for flake in snow_list:
-            flake.draw()
-            flake.fall(speed)
+        all_sprites.draw(screen)
 
         screen.fill((129, 193, 252), (0, 368, 1000, 100))
         screen.blit(winner, (250, 350))
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(30)
     pygame.quit()
